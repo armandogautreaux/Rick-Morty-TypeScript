0d5ab034c02fa710df6790578c0f4863c90d3e0b
Armando Gautreaux <armando.gautreaux@icloud.com>
2019-05-07 22:14:01 -0500

    Added hooks

--- src/Store.tsx
+++ src/Store.tsx
@@ -1,16 +1,32 @@
 import React from 'react';
+import { arrowFunctionExpression } from '@babel/types';
 
 interface IState {
   episodes: [];
   favourites: [];
 }
+
+interface IAction {
+  type: string;
+  payload: any;
+}
+
+//https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes
+
 const initialState: IState = {
   episodes: [],
   favourites: []
 };
 export const Store = React.createContext<IState>(initialState);
 
-function reducer() {}
+function reducer(state: IState, action: IAction) {
+  switch (action.type) {
+    case 'FETCH_DATA':
+      return { ...state, episodes: action.payload };
+    default:
+      return state;
+  }
+}
 
 export function StoreProvider(props: any): JSX.Element {
   return <Store.Provider value={initialState}>{props.children}</Store.Provider>;
